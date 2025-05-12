4fac5cc96b19e9170b03a0129960e832ae6c3f48
Chonmanee Chimpad <chonmanee.chi@gmail.com>
2025-05-12 22:47:57 +0700

    Update server.js

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -21,6 +21,7 @@ app.use('/api/meetings', meetingRoutes);
 app.use('/api/invite', inviteRoutes);
 app.use('/api/invitee', inviteAvailabilityRoutes);
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
