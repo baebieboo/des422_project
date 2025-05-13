2782b04813dc0185637e36225869ffa3765f8945
PorFerMat <pponglimagorn@gmail.com>
2025-05-13 10:04:19 +0700

    Update server.js

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // ✅ CORS middleware BEFORE any routes
 app.use(cors({
-  origin: 'https://des422-group5.onrender.com', // ⬅️ Replace this with your actual frontend Render URL
+  origin: 'https://my-react-app-wr93.onrender.com/', // ⬅️ Replace this with your actual frontend Render URL
   credentials: true
 }));
 
